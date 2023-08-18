98a96ddaaa7e9f84b8e2791d6e6ab480ce82af5f
Ivanfei <ivanfei2218@icloud.com>
2023-08-17 19:19:46 -0500

    add: multiple choice questions UI

--- lib/page/opentreehole/hole_exam.dart
+++ lib/page/opentreehole/hole_exam.dart
@@ -14,3 +14,59 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+import 'package:flutter/material.dart';
+
+class HoleExamPage extends StatelessWidget {
+  const HoleExamPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '发布违反社区公约的内容会导致被删帖/被禁言，比如以下哪种内容？',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ExamChoiceButton(text: '让大家关注嘉然'),
+            ExamChoiceButton(text: '提供学习建议'),
+            ExamChoiceButton(text: '发表对某位老师的不满'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExamChoiceButton extends StatelessWidget {
+  const ExamChoiceButton({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: OutlinedButton(
+            onPressed: () {},
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
