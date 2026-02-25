874d2de09a58eb85ece287d591680bf70c1f3171
Xiao Yang <24300240055@m.fudan.edu.cn>
2026-02-25 14:11:11 +0800

    清理临时验证目录

--- ohos_module_build/third_party/flutter_progress_dialog/ios/Classes/FlutterProgressDialogPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterProgressDialogPlugin : NSObject<FlutterPlugin>
-@end
--- ohos_module_build/third_party/flutter_progress_dialog/ios/Classes/FlutterProgressDialogPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterProgressDialogPlugin.h"
-#import <flutter_progress_dialog/flutter_progress_dialog-Swift.h>
-
-@implementation FlutterProgressDialogPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterProgressDialogPlugin registerWithRegistrar:registrar];
-}
-@end
